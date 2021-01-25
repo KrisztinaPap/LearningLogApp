05e2fb34d94df2c48ff90193b1e390db13cb8947
Krisztina Pap <krisztina.pap@outlook.com>
2021-01-25 15:27:37 -0700

    Added EF

---
+++ LearningLogApp/Models/LogContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningLogApp.Models
+{
+    public class LogContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public LogContext(DbContextOptions<LogContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ LearningLogApp/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LearningLogApp.Models
+{
+    public class Post
+    {
+        public long Id { get; set; }
+
+        private string _key;
+        public string Key
+        {
+            get
+            {
+                if(_key == null)
+                {
+                    _key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Subject { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
